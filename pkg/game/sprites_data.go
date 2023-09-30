182fb8415c3dac117be931caa17bbeee74f63871
Timo Reitz <godsboss@gmx.de>
2023-09-30 02:16:41 +0200

    Add (mock) background gfx.

--- pkg/game/sprites_data.go
+++ pkg/game/sprites_data.go
@@ -4,7 +4,22 @@ import "github.com/GodsBoss/gggg/v2/pkg/rendering/canvas2drendering"
 
 // spritesData contains a map of all the sprites this game uses.
 var spritesData = map[string]canvas2drendering.SpriteData{
-	"background_title":     {},
-	"background_playing":   {},
-	"background_game_over": {},
+	"background_title": {
+		X:      1600,
+		Y:      0,
+		Width:  320,
+		Height: 200,
+	},
+	"background_playing": {
+		X:      1600,
+		Y:      200,
+		Width:  320,
+		Height: 200,
+	},
+	"background_game_over": {
+		X:      1600,
+		Y:      400,
+		Width:  320,
+		Height: 200,
+	},
 }
