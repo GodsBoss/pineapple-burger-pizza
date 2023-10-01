6d315efbb35aa3b5a61b3127ee845189c3d67889
Timo Reitz <godsboss@gmx.de>
2023-10-01 15:47:41 +0200

    Move flavor stuff into separate file.

--- pkg/game/playing.go
+++ pkg/game/playing.go
@@ -399,33 +399,3 @@ type placedIngredient struct {
 	width       int
 	height      int
 }
-
-type flavor string
-
-const (
-	flavorSweet    flavor = "sweet"
-	flavorCalamari flavor = "calamari"
-	flavorSalty    flavor = "salty"
-	flavorFish     flavor = "fish"
-)
-
-// flavorList provides a consistent sort order for flavors. This is useful as maps don't have an inherent sort order.
-var flavorList = []flavor{
-	flavorCalamari,
-	flavorFish,
-	flavorSalty,
-	flavorSweet,
-}
-
-var ingredientFlavors = map[ingredientType]map[flavor]int{
-	ingredientAnanas: {
-		flavorSweet: 1,
-	},
-	ingredientAnchovi: {
-		flavorFish:  1,
-		flavorSalty: 1,
-	},
-	ingredientRubberBoots: {
-		flavorCalamari: 1,
-	},
-}
